551e6598a74004412d7afb443016865b37e7a570
alexangria1992 <alex040892@gmail.com>
2021-02-01 22:53:37 +1300

    HomeScreen Product Listing

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -10,8 +10,8 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                    <Nav.Link href="/cart"><i class="fa fa-shopping-cart"></i>Cart</Nav.Link>
-                    <Nav.Link href="/login"><i class="fa fa-user"></i>Login</Nav.Link>
+                    <Nav.Link href="/cart"><i className="fa fa-shopping-cart"></i>Cart</Nav.Link>
+                    <Nav.Link href="/login"><i className="fa fa-user"></i>Login</Nav.Link>
                   
                     </Nav>
              

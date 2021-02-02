5261683a8c67e3559eff1ad176633c21efd2d402
alexangria1992 <alex040892@gmail.com>
2021-02-02 16:45:52 +1300

    Product Details Screen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -12,12 +12,15 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
+
                     <LinkContainer to='/cart'>
                         <Nav.Link><i className="fa fa-shopping-cart"></i>Cart</Nav.Link>
                     </LinkContainer>
+
                     <LinkContainer to='/login'>
                         <Nav.Link><i className="fa fa-user"></i>Login</Nav.Link>
                     </LinkContainer>
+                    
                     </Nav>
              
                 </Navbar.Collapse>

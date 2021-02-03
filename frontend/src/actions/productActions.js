304b0b6f4d1c75c2c540448b64525d4ab1f7b5d0
alexangria1992 <alex040892@gmail.com>
2021-02-03 22:06:04 +1300

    Redux Into Home Screen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL} from '../constants/productConstants'
 
-const listProducts = () => async (dispatch) => {
+export const listProducts = () => async (dispatch) => {
     try {
         dispatch({type: PRODUCT_LIST_REQUEST })
         const {data} = await axios.get('/api/products/')

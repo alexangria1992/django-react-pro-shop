4335ff9ad5e206b9a570698b227789f6d65f487f
alexangria1992 <alex040892@gmail.com>
2021-02-11 22:14:04 +1300

    Signals (Re-Done)

--- backend/base/apps.py
+++ backend/base/apps.py
@@ -4,5 +4,5 @@ from django.apps import AppConfig
 class BaseConfig(AppConfig):
     name = 'base'
 
-#     def ready(self):
-#         import base.signals
+    def ready(self):
+        import base.signals

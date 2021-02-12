825b2b0a84e4d820009bf3b961ef0a6b460178fb
alexangria1992 <alex040892@gmail.com>
2021-02-12 13:37:54 +1300

    Fixed Login bug and Added Logout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,22 @@
 import React from 'react'
-import {Navbar, Nav, Container, Row } from 'react-bootstrap'
+import {useDispatch, useSelector} from 'react-redux'
+import {Navbar, Nav, Container, Row, NavDropdown} from 'react-bootstrap'
 import {LinkContainer} from 'react-router-bootstrap'
-const Header = () => {
+import {logout} from '../actions/userActions'
+
+function Header()
+{
+
+    const userLogin = useSelector(state => state.userLogin)
+    const {userInfo} = userLogin
+
+    const dispatch = useDispatch()
+
+    const logoutHandler = () =>
+    {
+        dispatch(logout())
+    }
+
     return (
         <header>
            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -17,9 +32,19 @@ const Header = () => {
                         <Nav.Link><i className="fa fa-shopping-cart"></i>Cart</Nav.Link>
                     </LinkContainer>
 
-                    <LinkContainer to='/login'>
-                        <Nav.Link><i className="fa fa-user"></i>Login</Nav.Link>
+                    {userInfo ? (
+                        <NavDropdown title={userInfo.name} id='username'>
+                            <LinkContainer to='/profile'>
+                                <NavDropdown.Item>Profile</NavDropdown.Item>
+                            </LinkContainer>
+
+                            <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                        </NavDropdown>
+                    ) : (
+                        <LinkContainer to='/login'>
+                        <Nav.Link><i class="fa fa-user" aria-hidden="true"></i>Login</Nav.Link>
                     </LinkContainer>
+                    )}
                     
                     </Nav>
              

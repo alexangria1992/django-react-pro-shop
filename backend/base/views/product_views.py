2d276b2593ed31a16f141de953dc2dbeab6b096a
alexangria1992 <alex040892@gmail.com>
2021-02-22 22:41:44 +1300

    Product Image Upload

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -63,3 +63,12 @@ def createProduct(request):
     )
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def uploadImage(request):
+    data = request.data 
+    product_id = data['product_id']
+    product = Product.objects.get(_id=product_id)
+    product.image = request.FILES.get('image')
+    product.save()
+    return Response('Image was uploaded')

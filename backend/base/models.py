aa47b3f6be5c6afd0c6873a2f626ada41b663007
alexangria1992 <alex040892@gmail.com>
2021-02-23 20:05:29 +1300

    Create Review Endpoint

--- backend/base/models.py
+++ backend/base/models.py
@@ -24,6 +24,7 @@ class Review(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
     rating = models.IntegerField(null=True, blank=True, default=0)
     comment = models.TextField(null = True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
     _id = models.AutoField(primary_key=True, editable = False)
 
     def __str__(self):
@@ -70,3 +71,4 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return str(self.address)
+
